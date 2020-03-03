c704223477526d1c3314a75449e69b33c16d77fc
7db9a <repo.7db9a@gmail.com>
2020-03-03 10:21:14 -0500

    feat(script): new add cmd.

--- content-addr.sh
+++ content-addr.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Imports PRIV_IPFS_STAGE, which is the path to staging directory.
+source env.sh
+
+send-file-to-ipfs() {
+    stage-file $1
+    ipfs-add $1
+}
+
 ipfs-cat() {
     docker exec \
     -it \
@@ -15,10 +23,26 @@ ipfs-add() {
     /export/$1
 }
 
-if [ "$1" == "cat" ]; then
+stage-file() {
+cp $1 $PRIV_IPFS_STAGE/
+}
+
+# Porcelain cmd
+if [ "$1" == "add" ]; then
+    send-file-to-ipfs $2
+fi
+
+# Plumbing cmd
+if [ "$1" == "ipfs-cat" ]; then
     ipfs-cat $2
 fi
 
-if [ "$1" == "add" ]; then
+# Plumbing cmd
+if [ "$1" == "ipfs-add" ]; then
     ipfs-add $2
+fi
+
+# Plumbing cmd
+if [ "$1" == "stage-file" ]; then
+    stage-file $2
 fi
