d9e4996ede0611d3663460c24cbfed3e45578859
7db9a <repo.7db9a@gmail.com>
2020-03-03 09:20:37 -0500

    feat(script): add ipfs add cmd.

--- content-addr.sh
+++ content-addr.sh
@@ -7,6 +7,18 @@ ipfs-cat() {
     ipfs cat $1
 }
 
+ipfs-add() {
+    docker exec \
+    -it \
+    ipfs_host \
+    ipfs add \
+    /export/$1
+}
+
 if [ "$1" == "cat" ]; then
     ipfs-cat $2
+fi
+
+if [ "$1" == "add" ]; then
+    ipfs-add $2
 fi
