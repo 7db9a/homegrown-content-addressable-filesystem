5114f0328ba5d9c2217bfe125330b0be4001382d
7db9a <repo.7db9a@gmail.com>
2020-03-04 13:55:34 -0500

    feat(script): back up node.

--- content-addr.sh
+++ content-addr.sh
@@ -3,6 +3,7 @@
 # You must add the PRIV_IPFS_STAGE environmental variable to your .bashrc (or the file for whichever shell you use).
 #
 # export PRIV_IPFS_STAGE=/path/to/private-ipfs-docker/private-network-ipfs/staging
+# export PRIV_IPFS_DATA=/path/to/private-ipfs-docker/private-network-ipfs/data
 #
 
 send-file-to-ipfs() {
@@ -28,6 +29,23 @@ ipfs-add() {
 stage-file() {
     cp $(pwd)/$1 $PRIV_IPFS_STAGE/$1
 }
+
+# Get your peers ip addresses.
+# $ docker exec ipfs_host ipfs swarm peers
+
+# rsync ipfs data to backup server.
+# You'll need ssh access into the server.
+
+rsync-node() {
+    # $1 is the user, $2 is the address, and $3 is the path to the backup.
+    # Example SRC DEST: data-dir /home/user/
+    # In the above example, dir is synced to /home/user/data-dir
+    rsync -azP --delete -e ssh $PRIV_IPFS_DATA $1@$2:$3
+}
+
+if [ "$1" == "backup" ]; then
+    rsync-node $2 $3 $4
+fi
 
 # Porcelain cmd
 if [ "$1" == "add" ]; then
