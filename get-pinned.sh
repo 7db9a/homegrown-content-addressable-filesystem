6f54a4f9595bc17665b088e75a291255195acb05
7db9a <repo.7db9a@gmail.com>
2020-03-06 23:17:21 -0500

    fix(script): accepts peer ID argument and touches to peer ID path.

--- get-pinned.sh
+++ get-pinned.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-PINNED_FILES=$(pwd)/pinned-files
+PEER_ID=$1
+
+PINNED_DIR=working/pinned-files/$PEER_ID
 
 ipfs-pinned() {
     docker exec ipfs_host ipfs pin ls --type=recursive -q
@@ -12,5 +14,5 @@ IFS=$'\r
 for i in "${PINNED[@]}"
 do
    # do whatever on $i
-   touch $PINNED_FILES/$i
+   touch $PINNED_DIR/$i
 done
